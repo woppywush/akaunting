db499dea83598b220fe214559a229167b326c4bd
woppywush <adimir@gmail.com>
2018-09-21 01:44:09 +0700

    Update edit.blade.php
    missing comma

--- resources/views/settings/settings/edit.blade.php
+++ resources/views/settings/settings/edit.blade.php
@@ -8,7 +8,7 @@
         {!! Form::model($setting, [
             'method' => 'PATCH',
             'url' => ['settings/settings'],
-            'class' => 'setting-form form-loading-button'
+            'class' => 'setting-form form-loading-button',
             'files' => true,
             'role' => 'form'
         ]) !!}
